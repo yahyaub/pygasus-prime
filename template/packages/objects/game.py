4a5bd3dfca1091dc72c0292dfabe7e0459590463
Yahya Bindir <yahyabindir@gmail.com>
2024-01-09 22:53:28 +0100

    Issue 4: Added random string to layer item names

--- template/packages/objects/game.py
+++ template/packages/objects/game.py
@@ -1,6 +1,9 @@
 import pygame
 from pygame.locals import *
 
+import random
+import string
+
 from abc import ABC, abstractmethod
 from packages.collision.collision import CollisionBox
 from packages.image.image import Image
@@ -8,7 +11,7 @@ from packages.image.image import Image
 class GameObject(ABC):
   def __init__(self, image_key, position, new_dimensions):
     image = Image.get(image_key)
-    self.name = image_key
+    self.name = image_key + self._get_random_string(4)
     self.image = Image.resize(image, new_dimensions)
     self.position = position
     self.is_active = True
@@ -16,6 +19,13 @@ class GameObject(ABC):
 
   def resize_collision_box(self, scale):
     self.box = CollisionBox(self, scale)
+
+  def change_name(self, name):
+    self.name = name
+
+  def _get_random_string(self, length):
+    alphanumeric_chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(alphanumeric_chars) for _ in range(length))
 
   @abstractmethod
   def update(self):
