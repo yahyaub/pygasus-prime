8e76008c8ccccde7361e5fbc5d62f95a318f6789
Yahya Bindir <yahyabindir@gmail.com>
2024-01-13 00:24:32 +0100

    Issue 14: Added a few more grid methods

--- template/packages/display/grid.py
+++ template/packages/display/grid.py
@@ -5,5 +5,33 @@ from packages.value.constants import GRID_DIM
 
 class Grid:
   @staticmethod
-  def snap(pos):
-    return (pos[0]*GRID_DIM, pos[1]*GRID_DIM)
+  def number_to_pixel(number):
+    return number*GRID_DIM
+
+  @staticmethod
+  def pixel_to_number(pixel):
+    return pixel // GRID_DIM
+
+  @staticmethod
+  def list_number_to_pixel(number_list):
+    pixel_list = []
+    for number in number_list:
+      pixel_list.append(Grid.number_to_pixel(number))
+
+    return pixel_list
+
+  @staticmethod
+  def list_pixel_to_number(pixel_list):
+    number_list = []
+    for pixel in pixel_list:
+      number_list.append(Grid.pixel_to_number(pixel))
+
+    return number_list
+
+  @staticmethod
+  def tuple_number_to_pixel(number_tuple):
+    return tuple(Grid.list_number_to_pixel(list(number_tuple)))
+
+  @staticmethod
+  def tuple_pixel_to_number(pixel_tuple):
+    return tuple(Grid.list_pixel_to_number(list(pixel_tuple)))
