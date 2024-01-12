69a814de07dfa82cd7aa77fc448d3ae20b5827b8
Yahya Bindir <yahyabindir@gmail.com>
2024-01-13 00:07:48 +0100

    Issue 18: Added more random methods

--- template/packages/objects/game.py
+++ template/packages/objects/game.py
@@ -1,17 +1,15 @@
 import pygame
 from pygame.locals import *
 
-import random
-import string
-
 from abc import ABC, abstractmethod
 from packages.collision.collision import CollisionBox
 from packages.image.image import Image
+from packages.value.random import Random
 
 class GameObject(ABC):
   def __init__(self, image_key, position, new_dimensions):
     image = Image.get(image_key)
-    self.name = image_key + self._get_random_string(4)
+    self.name = image_key + Random.alphanumeric(4)
     self.image = Image.resize(image, new_dimensions)
     self.position = position
     self.is_active = True
@@ -20,10 +18,6 @@ class GameObject(ABC):
 
   def change_name(self, name):
     self.name = name
-
-  def _get_random_string(self, length):
-    alphanumeric_chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(alphanumeric_chars) for _ in range(length))
 
   @abstractmethod
   def update(self):
--- template/packages/value/random.py
+++ template/packages/value/random.py
@@ -2,10 +2,19 @@ import pygame
 from pygame.locals import *
 
 import random
+import string
 
 class Random:
   @staticmethod
-  def number(start, stop):
+  def number(stop):
+    return Random.number_between(1, stop)
+
+  @staticmethod
+  def index(stop):
+    return Random.number_between(0, stop)
+
+  @staticmethod
+  def number_between(start, stop):
     random.seed()
     return random.randint(start, stop)
 
@@ -14,3 +23,8 @@ class Random:
     random.seed()
     index = random.randint(0, len(list)-1)
     return list[index]
+
+  @staticmethod
+  def alphanumeric(length):
+    alphanumeric_chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(alphanumeric_chars) for _ in range(length))
