ac5c8958901e2a93b32dd0e67413da4c3302f71b
Yahya Bindir <yahyabindir@gmail.com>
2025-03-02 14:55:48 +0100

    Added templating for smoother startup

--- new_game.sh
+++ new_game.sh
@@ -2,7 +2,7 @@
 
 # Check if the correct number of arguments is provided
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <new-directory>"
+  echo "Usage: $0 <project-name>"
   exit 1
 fi
 
@@ -12,10 +12,32 @@ new_directory="$1"
 # Create the new directory and its parent directories if they don't exist
 mkdir -p "$new_directory"
 
-# Copy the contents of the current directory to the new directory
+# Copy the contents of the template folder into the new directory
 cp -R ./template/* "$new_directory"
 
-# Remove the .git folder and .gitignore file from the new directory
+# Remove .git and __pycache__ folders if they exist
 rm -rf "$new_directory/.git" "$new_directory/.gitignore"
-
 find "$new_directory" -type d -name "__pycache__" -exec rm -rf {} +
+
+# Run render.py to generate the templated files
+python ./file-templates/render.py "$new_directory"
+
+# Extract the processed project name (same logic as render.py)
+project_words=$(echo "$new_directory" | grep -oE '[a-zA-Z]+')
+processed_name=""
+for word in $project_words; do
+    processed_name+=$(echo "$word" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+done
+processed_name_lower=$(echo "$processed_name" | awk '{print tolower($0)}')
+
+# Move generated files into the new directory
+mv "./file-templates/${processed_name_lower}.py" "$new_directory/core/display/${processed_name_lower}.py"
+rm "$new_directory/core/display/blank.py"
+mv "./file-templates/starter.py" "$new_directory/core/game/starter.py"
+cp "Pipfile" "$new_directory"
+mv "$new_directory" "../$new_directory"
+
+echo "Project '$new_directory' successfully set up!"
+
+echo "Press any key to continue..."
+read -n 1 -s
